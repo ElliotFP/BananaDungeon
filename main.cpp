b8fa0727c14cf3ceb1cc3a8227bdb1278cb5ffd1
Elliot Forcier-Poirier <elliot.forcier-poirier@mail.mcgill.ca>
2023-05-27 19:17:27 -0400

    Lesson 1: Banana Dungeon instead of Hello World

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// extra library includes
+#include <iostream> //input output stream
+#include <string>   //string library
+
+using namespace std; // standard namespace
+
+int main()
+{
+    // all of our starter code will start here
+
+    // output Banana Dungeon to the screen
+    cout << "Banana Dungeon" << endl; // cout is short for console out and endl is short for end line
+
+    system("pause"); // pause the program so we can see the output
+}
